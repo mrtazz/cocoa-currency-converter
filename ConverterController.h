7cd69466fd54e121ed530603e246f0bac83196e6
Daniel Schauenberg <d@unwiredcouch.com>
2010-07-12 08:40:52 +0200

    add Controller Class

---
+++ ConverterController.h
@@ -0,0 +1,15 @@
+//
+//  ConverterController.h
+//  cocoa-example-app
+//
+//  Created by Daniel Schauenberg on 7/12/10.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ConverterController : NSObject {
+
+}
+
+@end
---
+++ ConverterController.m
@@ -0,0 +1,14 @@
+//
+//  ConverterController.m
+//  cocoa-example-app
+//
+//  Created by Daniel Schauenberg on 7/12/10.
+//  Copyright 2010 Steinbeis Transfercenter for Embedded Design And Networking. All rights reserved.
+//
+
+#import "ConverterController.h"
+
+
+@implementation ConverterController
+
+@end
