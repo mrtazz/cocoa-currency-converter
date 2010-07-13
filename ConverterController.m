f0895a13065abc3b0438550c09a7eac8e47d50bd
Daniel Schauenberg <d@unwiredcouch.com>
2010-07-13 08:25:53 +0200

    remove copyright comment

--- ConverterController.m
+++ ConverterController.m
@@ -3,7 +3,6 @@
 //  cocoa-example-app
 //
 //  Created by Daniel Schauenberg on 7/12/10.
-//  Copyright 2010 Steinbeis Transfercenter for Embedded Design And Networking. All rights reserved.
 //
 
 #import "ConverterController.h"
