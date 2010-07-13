723514b1d4338112dbe3788f63f2b65b75537bbb
Daniel Schauenberg <d@unwiredcouch.com>
2010-07-13 08:26:17 +0200

    add Outlets and convert method to header file

--- ConverterController.h
+++ ConverterController.h
@@ -9,7 +9,11 @@
 
 
 @interface ConverterController : NSObject {
-
+  IBOutlet NSTextField *amountField;
+  IBOutlet NSTextField *dollarField;
+  IBOutlet NSTextField *rateField;
 }
+
+- (IBAction)convert:(id)sender;
 
 @end
