a123fef8681f94aaedaf002b777fc7a35b182e88
Daniel Schauenberg <d@unwiredcouch.com>
2010-07-15 19:39:56 +0200

    add implementation for controller

--- ConverterController.m
+++ ConverterController.m
@@ -9,5 +9,18 @@
 
 
 @implementation ConverterController
+- (IBAction)convert:(id)sender
+{
+  float amount;
+  converter = [[Converter alloc]init];
 
+  [converter setSourceCurrencyAmount:[dollarField floatValue]];
+
+  [converter setRate:[rateField floatValue]];
+  amount = [converter convertCurrency];
+
+  [amountField setFloatValue:amount];
+
+  [rateField selectText:self]
+}
 @end
